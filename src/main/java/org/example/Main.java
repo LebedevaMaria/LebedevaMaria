2025ab20847d4237068a3d739fd860d69cc8626d
Lebedeva Maria <lebedeva.mariya.05@gmail.com>
2023-10-05 20:15:58 +0300

    Push unit test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,11 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        Animals animal = new Animals();
-        animal.animal = "Лошадь";
-        animal.typeAnimal();
-        animal.moving();
-        String[] food = {"Трава", "Мясо"};
-        animal.eating(food);
+
     }
 }
