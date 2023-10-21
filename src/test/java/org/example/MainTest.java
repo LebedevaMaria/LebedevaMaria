f4911ce7a4decd510bb669a5109fa6441710276f
Lebedeva Maria <lebedeva.mariya.05@gmail.com>
2023-10-21 14:12:15 +0300

    Push

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -1,11 +1,24 @@
 package org.example;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MainTest {
     @Test
-    void testSum() {
-        assertEquals(2, 1 + 1);
+    void TestMerge(){
+        var ans = new ArrayList<Integer>(Arrays.stream(new int[]{3, 5, 7, 9}).boxed().toList());
+        var qua = new ArrayList<Integer>(Arrays.stream(new int[]{9, 5, 3, 7}).boxed().toList());
+        Sort.sort(qua, "mergeSort");
+        assert ans.equals(qua);
+    }
+
+    @Test
+    void TestBubble(){
+        var ans = new ArrayList<Integer>(Arrays.stream(new int[]{3, 5, 7, 9}).boxed().toList());
+        var qua = new ArrayList<Integer>(Arrays.stream(new int[]{9, 5, 3, 7}).boxed().toList());
+        Sort.sort(qua, "bubbleSort");
+        assert ans.equals(qua);
     }
 }
